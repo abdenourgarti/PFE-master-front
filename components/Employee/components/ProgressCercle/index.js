63e8ebdd29f36a3fa741f38274de27110cefd6c8
Abdenour Garti <garti.abdenour@gmail.com>
2024-10-14 16:59:07 +0100

    add react-circle

--- components/Employee/components/ProgressCercle/index.js
+++ components/Employee/components/ProgressCercle/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-//import Circle from 'react-circle';
+import Circle from 'react-circle';
 
 const ProgressCircle = ({ completed, total }) => {
   const percentage = (total === 0) ? "0.00" : ((completed / total) * 100).toFixed(2);
 
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
-      {/*<Circle
+      <Circle
         progress={percentage}
         progressColor="blue"
         showPercentValue={true}
         showPercentValueDecimalPlaces={2}
         size={200}
-      />*/}
+      />
       <div style={{ marginLeft: '20px' }}>
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <div style={{ width: '10px', height: '10px', backgroundColor: 'blue', marginRight: '5px' }}></div>
